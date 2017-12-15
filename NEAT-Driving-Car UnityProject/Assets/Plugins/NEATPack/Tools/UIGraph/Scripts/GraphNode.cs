b34d07ba3868ad07da4910a78afeb5be7f11efaf
Unicornslayer <terman.emil@gmail.com>
2017-12-15 21:00:10 +0200

    Removed ssome trash

--- NEAT-Driving-Car UnityProject/Assets/Plugins/NEATPack/Tools/UIGraph/Scripts/GraphNode.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-
-[RequireComponent(typeof(RectTransform)),
- SelectionBase]
-public class GraphNode : MonoBehaviour {}
--- NEAT-Driving-Car UnityProject/Assets/Plugins/NEATPack/Tools/UIGraph/Scripts/Util/ObfuscateObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ObfuscateObject : MonoBehaviour {
-	[ContextMenu("Obfuscate")]
-	public void Obfuscate() {
-		gameObject.name = gameObject.name.GetHashCode().ToString();
-	}
-}
